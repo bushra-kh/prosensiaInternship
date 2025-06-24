3e9fa7d9652283e3386e00ebf4456a9bce5d5df2
bushra-kh <bushrakhaled.official@gmail.com>
2025-06-24 13:31:37 +0500

    task 1

---
+++ task 1.py
@@ -0,0 +1,14 @@
+# 1. Takes three inputs from the user:
+# An integer
+# A float
+# A string
+# 2. Store each value in a separate variable
+# 3. Use typecasting if needed
+# 4. Display the values using formatted print statements
+float_input=float(input("Enter a float input:"))
+int_input=int(input("Enter a integer input:"))
+string_input=input("Enter a string input:")
+
+print("Integer:", str(int_input))
+print("Float:", str(float_input))
+print("String:", string_input)
